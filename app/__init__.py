c7cabda7a8c3840f4340b085841ae906f1d5ddc1
Gheorghe Mirca <giony.mirca@gmail.com>
2024-06-24 21:31:01 +0300

    added anotation for create app function

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,10 @@ from flask_restful import Api
 from app.api import Personal, Experience, Education
 from app.cli import register_commands
 
-def create_app():
+def create_app() -> Flask:
     app = Flask(__name__)
     api = Api(app)
-    
+
     api.add_resource(Personal, '/personal')
     api.add_resource(Experience, '/experience')
     api.add_resource(Education, '/education')

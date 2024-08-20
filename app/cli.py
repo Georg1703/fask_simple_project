a03499ab7b2268d95cb7e9f8ea20b0f41b2f9bb6
Gheorghe <86838874+Georg1703@users.noreply.github.com>
2024-08-20 03:52:49 +0000

    test

--- app/cli.py
+++ app/cli.py
@@ -6,5 +6,5 @@ from app.data import CV_DATA
 def register_commands(app):
     @app.cli.command("cv-info")
     def show_cv_info() -> None:
-        """Displays the CV data in the console."""
+        """Displays the CV data in the console.."""
         print(json.dumps(CV_DATA))
